47c718464825b56f8afd1a53d205a89180341071
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 08:22:18 +0530

    Added: JS code to covert a digit to word.

--- coinFlip.js
+++
@@ -1,7 +0,0 @@
-let a=Math.floor((Math.random()%10)*2)
-
-if(a==1)
-    console.log("Got Head.")
-else
-    console.log("Got Tail.")    
-    
---
+++ digitToWord.js
@@ -0,0 +1,38 @@
+let dig=prompt("Enter a single digit number:");
+
+if(dig==0)
+    console.log("Zero")
+else
+if(dig==1)
+    console.log("One")
+else
+if(dig==2)
+    console.log("Two")
+else
+if(dig==3)
+    console.log("Three")
+else
+if(dig==4)
+    console.log("Four")
+else
+if(dig==5)
+    console.log("Five")
+else
+if(dig==6)
+    console.log("Six")
+else
+if(dig==7)
+    console.log("Seven")
+else
+if(dig==8)
+    console.log("Eight")
+else
+if(dig==9)
+    console.log("Nine")
+    
+    
+    
+    
+    
+    
+        
