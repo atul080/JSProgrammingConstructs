b01a098a69c6da30ddf551de4be221f9ecf2344d
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 08:24:09 +0530

    Added: JS code to get week day by taking number input.

---
+++ weekDay.js
@@ -0,0 +1,22 @@
+let dig=prompt("Enter a single digit number greater than 0 and less that 8:");
+
+if(dig==1)
+    console.log("Sunday")
+else
+if(dig==2)
+    console.log("Monday")
+else
+if(dig==3)
+    console.log("Tuesday")
+else
+if(dig==4)
+    console.log("Wednesday")
+else
+if(dig==5)
+    console.log("Thursday")
+else
+if(dig==6)
+    console.log("Friday")
+else
+if(dig==7)
+    console.log("Saturday")
