469a01146c5068520654a06a1a100392dbb388ce
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 08:23:22 +0530

    Added: JS code to check if digit is Uit, Tens or Hundreds.

---
+++ UnitTenHunDigit.js
@@ -0,0 +1,13 @@
+let dig=prompt("Enter a digit:");
+
+    if(dig>=0 && dig<=9)
+        console.log("Unit digits.")
+    else
+    if(dig>=10 && dig<=99)
+        console.log("Ten digit.")
+    else
+    if(dig>=100 && dig<=999)
+        console.log("Hundred digit.")
+        
+
+
