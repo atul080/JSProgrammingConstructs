8686a8e6196b58cf087ee9b7e4bdb6179c8a51a7
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 08:20:18 +0530

    Added: JS code to find if givn date is btween March 20 and June20 or not.

---
+++ dayBtwnRange.js
@@ -0,0 +1,15 @@
+const day=prompt("Enter day in DD format.");
+const month=prompt("Enter month in MM format.");
+
+if((day>19)&&(month==3)&&(day<=31))
+    console.log("Date is between March 20 and June 20.")
+else
+if((day>0)&&(month==4)&&(day<=30))    
+    console.log("Date is between March 20 and June 20.")
+else
+if((day>0)&&(month==5)&&(day<=31))    
+    console.log("Date is between March 20 and June 20.")
+else 
+if((day>0)&&(month==6)&&(day<=20))    
+    console.log("Date is between March 20 and June 20.")
+
